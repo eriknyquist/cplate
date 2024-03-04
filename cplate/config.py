9555c5501f5e868864d6ab360daf225195150cf1
Erik Nyquist <eknyquist@gmail.com>
2024-03-03 23:29:29 -0800

    Start working on CLI interface, gluing stuff together

--- cplate/config.py
+++ cplate/config.py
@@ -4,13 +4,19 @@ import configparser
 from dataclasses import dataclass, field
 from typing import Dict
 
+
 DEFAULT_CONFIG_FILE = os.path.join(os.path.expanduser("~"), ".cplate_config.ini")
+
+BRACE_STYLES = ["allman", "knr"]
 
 
 @dataclass
 class CplateConfig:
+    filename = ''
+
     header_comment_file: str = ''
-    author: str = ''
+    header_comment: str = ''
+    author: str = 'Author name'
     doxygen_comments: bool = True
     include_guards: bool = True
     blank_line_count: int = 2
@@ -18,14 +24,25 @@ class CplateConfig:
     custom_return_values: dict = field(default_factory=dict)
 
     @classmethod
-    def from_string(cls, string):
+    def _err(cls, msg):
+        if cls.filename:
+            msg = f"Error in {cls.filename}: {msg}"
+
+        raise ValueError(msg)
+
+    @classmethod
+    def from_string(cls, string, filename=None):
+        if filename:
+            cls.filename = filename
+
         config = configparser.ConfigParser()
         config.read_string(string)
         ret = CplateConfig()
 
         if 'comments' in config:
             if 'header_comment_file' in config['comments']:
-                ret.header_comment_file = config['comments']['header_comment_file'].strip()
+                filepath = config['comments']['header_comment_file'].strip()
+                ret.header_comment_file = os.path.normpath(os.path.expanduser(filepath))
 
             if 'author' in config['comments']:
                 ret.author = config['comments']['author']
@@ -34,18 +51,22 @@ class CplateConfig:
                 try:
                     ret.doxygen_comments = config['comments'].getboolean('doxygen_comments')
                 except ValueError:
-                    raise ValueError("true/false required for comments.doxygen_comments")
+                    cls._err("true/false required for comments.doxygen_comments")
 
         if 'whitespace' in config:
             if 'blank_line_count' in config['whitespace']:
                 try:
                     ret.blank_line_count = config['whitespace'].getint('blank_line_count')
                 except ValueError:
-                    raise ValueError("Integer required for whitespace.blank_line_count")
+                    cls._err("Integer required for whitespace.blank_line_count")
 
         if 'code' in config:
             if 'brace_style' in config['code']:
-                ret.brace_style = config['code']['brace_style']
+                brace_style = config['code']['brace_style'].lower()
+                if brace_style not in BRACE_STYLES:
+                    cls._err(f"Invalid brace style '{brace_style}', valid styles are: {BRACE_STYLES}")
+
+                ret.brace_style = brace_style
 
             if 'include_guards' in config['code']:
                 ret.include_guards = config['code'].getboolean('include_guards')
@@ -59,7 +80,7 @@ class CplateConfig:
     @classmethod
     def from_file(cls, filename):
         with open(filename, 'r') as fh:
-            return cls.from_string(fh.read())
+            return cls.from_string(fh.read(), filename)
 
     def to_string(self):
         config = configparser.ConfigParser()
--- cplate/generator.py
+++ cplate/generator.py
@@ -11,15 +11,20 @@ HEADER_COMMENT = """
 """
 
 
-def _default_returnval(func_spec):
+def _default_returnval(func_spec, config):
     try:
         return spec.DEFAULT_VALS[func_spec.return_type]
     except KeyError:
         raise ValueError(f"Invalid function return type '{func_spec.return_type}'")
 
 
-def _c_file_contents(filename, func_specs, h_filename=None):
-    ret = HEADER_COMMENT.format(filename).lstrip() + "\n\n"
+def _c_file_contents(filename, func_specs, config, h_filename=None):
+    ret = ""
+
+    if config.header_comment:
+        ret += config.header_comment + ("\n" * config.blank_line_count)
+
+    ret += HEADER_COMMENT.format(filename).lstrip() + ("\n" * config.blank_line_count)
 
     impls = []
 
@@ -44,7 +49,7 @@ def _c_file_contents(filename, func_specs, h_filename=None):
     return ret
 
 
-def _doxygen_docs(func_spec):
+def _doxygen_docs(func_spec, config):
     ret = "/**\n * Function description\n"
 
     longest_argname_len = 0
@@ -71,8 +76,13 @@ def _doxygen_docs(func_spec):
 
     return ret
 
-def _h_file_contents(filename, func_specs):
-    ret = HEADER_COMMENT.format(filename).lstrip() + "\n\n"
+def _h_file_contents(filename, func_specs, config):
+    ret = ""
+
+    if config.header_comment:
+        ret += config.header_comment + ("\n" * config.blank_line_count)
+
+    ret += HEADER_COMMENT.format(filename).lstrip() + "\n\n"
 
     guard_name = filename.replace('.', '_').upper()
     ret += f'#ifndef {guard_name}\n'
@@ -81,7 +91,7 @@ def _h_file_contents(filename, func_specs):
     impls = []
 
     for func_spec in func_specs:
-        impl = _doxygen_docs(func_spec) + "\n" + func_spec.signature() + ";\n\n"
+        impl = _doxygen_docs(func_spec, config) + "\n" + func_spec.signature() + ";\n\n"
         impls.append(impl)
 
     ret += '\n'.join(impls)
@@ -89,18 +99,18 @@ def _h_file_contents(filename, func_specs):
     return ret
 
 
-def generate_c_module(c_filename, h_filename, spec_lines=[''], root_dir='.'):
+def generate_c_module(c_filename, h_filename, config, spec_lines=[''], root_dir='.'):
     func_specs = [spec.FunctionSpec.from_string(line) for line in spec_lines]
 
     if c_filename:
         c_filepath = os.path.join(root_dir, c_filename)
         with open(c_filepath, 'w') as fh:
-            fh.write(_c_file_contents(c_filename, func_specs, h_filename))
+            fh.write(_c_file_contents(c_filename, func_specs, config, h_filename))
 
     if h_filename:
         h_filepath = os.path.join(root_dir, h_filename)
         with open(h_filepath, 'w') as fh:
-            fh.write(_h_file_contents(h_filename, func_specs))
+            fh.write(_h_file_contents(h_filename, func_specs, config))
 
 
 if __name__ == "__main__":
